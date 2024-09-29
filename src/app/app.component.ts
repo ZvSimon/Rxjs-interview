7aa16972d06461906af3f0005476cfeecf4a1c48
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-29 20:11:54 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,94 +1,35 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {filter, from, of, tap, toArray} from "rxjs";
+import {delay, from, map, mergeMap, Observable, of} from "rxjs";
 
-
-export interface UserInterface {
-  id: number;
-  name: string;
-  age: number;
-  isActive: boolean;
+export interface UserInterface{
+  id:string,
+  name:string,
+  age:number
 }
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  styleUrls: ['./app.component.css']
 })
 
-/**
- export class AppComponent {
- constructor() {
- const users: UserInterface[] = [
- {id: 1, name: 'John', age: 28,isActive: true},
- {id: 2, name: 'Jane', age: 32,isActive: true},
- {id: 3, name: 'Jack', age: 25,isActive: false},
- ];
- const users$ = of(users);
- users$.pipe(
- map(users=> users.filter(user=>user.isActive)),
- tap(console.log)
- ).subscribe()
- }
- }**/
-//output : Array [ {…}, {…} ]
-// ​
-// 0: Object { id: 1, name: "John", age: 28, … }
-// ​
-// 1: Object { id: 2, name: "Jane", age: 32, … }
-// ​
-// length: 2
-//  export class AppComponent {
-//  constructor() {
-//  const users: UserInterface[] = [
-//  {id: 1, name: 'John', age: 28,isActive: true},
-//  {id: 2, name: 'Jane', age: 32,isActive: true},
-//  {id: 3, name: 'Jack', age: 25,isActive: false},
-//  ];
-//  const users$ = from(users);
-//  users$.pipe(
-// filter(users=>users.isActive),
-//  tap(console.log)
-//  ).subscribe()
-//  }
-//  //output :
-//   //Object { id: 1, name: "John", age: 28, isActive: true }
-//   // Object { id: 2, name: "Jane", age: 32, isActive: true }
-//  }
-
-/**
- export class AppComponent {
- constructor() {
- const users: UserInterface[] = [
- {id: 1, name: 'John', age: 28, isActive: true},
- {id: 2, name: 'Jane', age: 32, isActive: true},
- {id: 3, name: 'Jack', age: 25, isActive: false},
- ];
- const users$ = of(users);
- users$.pipe(
- filter(users => users.every(user => user.isActive)),
- tap(console.log)
- ).subscribe()
- }
- }**/
-// Ne va rien emettre car un user est false
 
 export class AppComponent {
-  constructor() {
-    const users: UserInterface[] = [
-      {id: 1, name: 'John', age: 28, isActive: true},
-      {id: 2, name: 'Jane', age: 32, isActive: true},
-      {id: 3, name: 'Jack', age: 25, isActive: false},
-    ];
-    const users$ = from(users);
-    users$.pipe(
-      filter(users=> users.isActive),
-      toArray(),
-      tap(console.log)
-    ).subscribe()
-  }
+constructor() {
+  const example = (operator: any) => () => {
+    from([0, 1, 2, 3, 4])
+      .pipe(operator((x: any) => of(x).pipe(delay(200))))
+      .subscribe(
+        console.log,
+        () => {},
+        () => console.log(`${operator.name} completed`)
+      );
+  };
+
+  example(mergeMap)();
 }
 
-
+}
